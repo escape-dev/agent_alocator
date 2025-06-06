bd62db95e1a769998ed35a4df4d4aabaac0e1275
Muhammad Hafiz <hafiz170301@gmail.com>
2025-06-06 15:40:24 +0700

    feat: create filter available agents

--- app/services/agent.rb
+++ app/services/agent.rb
@@ -1,10 +1,20 @@
 class Agent
   MAX_CUSTOMERS_PER_AGENTS = Master.first&.max_customer_per_agent || 2
 
+  def self.available_agents
+    agents = ApiClient.get_agents
+    agents.select { |agent| available?(agent) }
+  end
+
   def self.find_available
-    agents = ApiClient.get_available_agents
-    agents
-      .select { |agent| agent[:current_customer_count] < MAX_CUSTOMERS_PER_AGENTS }
-      .min_by { |agent| agent[:current_customer_count] }
+    available_agents.min_by { |agent| agent["current_customer_count"] }
+  end
+
+  private
+
+  def self.available?(agent)
+    !agent["force_offline"] &&
+    agent["is_available"] &&
+    agent["current_customer_count"] < MAX_CUSTOMERS_PER_AGENTS
   end
 end
